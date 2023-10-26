ed21faeabe57abe1e9f35e60b2ecd69c049cf256
hozan <>
2023-10-26 17:24:45 +0800

    build: update native config

--- android/src/main/java/com/perfectlib/PerfectLibModule.java
+++ android/src/main/java/com/perfectlib/PerfectLibModule.java
@@ -6,7 +6,7 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactMethod;
 
-public class PerfectLibModule extends PerfectLibSpec {
+public class PerfectLibModule extends com.perfectlib.PerfectLibSpec {
   public static final String NAME = "PerfectLib";
 
   PerfectLibModule(ReactApplicationContext context) {
